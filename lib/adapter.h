52a6cb412220a9849abff0134bc492e893e0e50a
Rajat Khandelwal <rajat.khandelwal@intel.com>
2023-07-07 12:58:29 +0530

    Change the license for 'lstbt' software to GPL 2.0
    GPL 3.0 -> GPL 2.0
    
    Signed-off-by: Rajat Khandelwal <rajat.khandelwal@intel.com>

--- lib/adapter.h
+++ lib/adapter.h
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: GPL-2.0
 
 /*
  * Copyright (C) 2023 Rajat Khandelwal <rajat.khandelwal@intel.com>
--- lib/helpers.c
+++ lib/helpers.c
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: GPL-2.0
 /*
  * Helpers for the user-space library (lstbt)
  *
